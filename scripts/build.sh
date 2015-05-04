9d073529e3fae8efa02dc7df7422173ccb638799
Gabor Szarnyas <szarnyasg@gmail.com>
2015-05-04 11:24:06 +0200

    Removed MaxPermSize option as it is no longer supported by Java 8

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,13 +1,3 @@
-#*******************************************************************************
-# Copyright (c) 2010-2014, Gabor Szarnyas, Istvan Rath and Daniel Varro
-# All rights reserved. This program and the accompanying materials
-# are made available under the terms of the Eclipse Public License v1.0
-# which accompanies this distribution, and is available at
-# http://www.eclipse.org/legal/epl-v10.html
-#
-# Contributors:
-# Gabor Szarnyas - initial API and implementation
-#*******************************************************************************
 #!/bin/bash
 
 cd "$( cd "$( dirname "$0" )" && pwd )/.."
@@ -21,7 +11,7 @@ while [ "$1" != "" ]; do
 	shift
 done
 
-export MAVEN_OPTS="-Xmx512m -XX:MaxPermSize=256m"
+export MAVEN_OPTS="-Xmx512m"
 
 if [ $skipTests ]; then
 	mvn clean install -DskipTests || exit 1
