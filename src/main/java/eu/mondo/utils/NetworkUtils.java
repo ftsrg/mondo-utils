b9752d44fb62166ad5987eee203589be39fa7089
Dénes Harmath <harmathdenes@gmail.com>
2015-09-01 13:39:00 +0200

    Fix NetworkUtils.getLocalIpAddress

--- src/main/java/eu/mondo/utils/NetworkUtils.java
+++ src/main/java/eu/mondo/utils/NetworkUtils.java
@@ -1,15 +1,13 @@
 package eu.mondo.utils;
 
 import java.io.IOException;
-import java.net.Socket;
+import java.net.InetAddress;
 
 public class NetworkUtils {
 	
 	public static String getLocalIpAddress() throws IOException{
-		Socket socket = new Socket("google.com", 80);
-		String result = socket.getLocalAddress().getHostAddress();
-		socket.close();
-		return result;
+		String hostName = InetAddress.getLocalHost().getHostName();
+	    return InetAddress.getByName(hostName).getHostAddress();
 	}
 
 }
